1665f4279bd7a8b6e56d4a2a79b822a17389f9fb
Louis Ponet <louisponet@gmail.com>
2023-09-08 18:32:12 +0200

    removed --cov-report

--- tests/tests.sh
+++ tests/tests.sh
@@ -3,4 +3,5 @@ set -ev
 
 # Make sure the folder containing the workchains is in the python path before the daemon is started
 verdi daemon start 4
-AIIDA_TEST_PROFILE=test_aiida pytest -vvv --cov-report=xml
+AIIDA_TEST_PROFILE=test_aiida pytest -vvv
+#--cov-report=xml
