c1edb2da4c9418e969be0886f40c9f138543db85
Louis Ponet <louisponet@gmail.com>
2023-09-08 18:25:43 +0200

    added PLACEHOLDER_WORK_DIR

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -2,8 +2,10 @@
 set -ev
 
 CONFIG="${GITHUB_WORKSPACE}/.github/config"
-
+sed -i "s|PLACEHOLDER_WORK_DIR|${GITHUB_WORKSPACE}|" "${CONFIG}/localhost.yaml"
 verdi setup --non-interactive --config "${CONFIG}/profile.yaml"
 
 # set up localhost computer
 verdi computer setup --non-interactive --config "${CONFIG}/localhost.yaml"
+verdi computer configure core.local localhost --config "${CONFIG}/localhost-config.yaml"
+verdi computer test localhost
