12aea3f8a8e7154d9031c7290d04b414a9b4e232
Louis Ponet <louisponet@gmail.com>
2023-09-08 18:38:09 +0200

    reentry needed?

--- tests/tests.sh
+++ tests/tests.sh
@@ -3,5 +3,4 @@ set -ev
 
 # Make sure the folder containing the workchains is in the python path before the daemon is started
 verdi daemon start 4
-AIIDA_TEST_PROFILE=test_aiida pytest -vvv
-#--cov-report=xml
+AIIDA_TEST_PROFILE=test_aiida pytest -vvv --cov-report=xml
