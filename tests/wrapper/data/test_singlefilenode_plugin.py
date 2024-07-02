5da70d1b0255ac66fbcf3aadfd1c1f136dabe7a0
Francesca.L.Bleken@sintef.no <francesca.l.bleken@sintef.no>
2024-07-02 08:46:20 +0200

    Added test which does things one by one

---
+++ tests/wrapper/data/test_singlefilenode_plugin.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import dlite
+
+# This seems to append entities dir to dlite.storage_path
+from execflow.data.singlefile_converter import singlefile_converter
+
+testdir = Path(__file__).resolve().parent.parent.parent
+pkgdir = testdir.parent
+sampledir = testdir / "samples"
+
+
+# Basic
+DataModel = dlite.get_instance("http://onto-ns.com/meta/2.0/core.singlefile")
+
+
+# Step 2
+sample = sampledir / "dlite_instance_reaction.json"
+with Path.open(sample, "rb") as f:
+    content = f.read()
+
+singlefileinst = DataModel(dimensions=(len(content),))
+singlefileinst.content = content
+
+
+# save the instance as a file to use the singlefiledatanode plugin directly
+singlefileinst.save("json", "instance.json", options="mode=w")
+# load it back inn
+# inst0 = dlite.Instance.from_location("json",
+#                                      "instance.json")
+
+
+# step 3, load the instance directly from file
+dlite.storage_path.append(sampledir / "DLiteDataModelReaction.json")
+dlite.python_storage_plugin_path.append(pkgdir / "execflow" / "data" / "dlite_plugins")
+# load with json after havin made singlenodefile available
+inst1 = dlite.Instance.from_location("json", "instance.json")
+
+
+inst2 = dlite.Instance.from_location("singlefiledatanode", "instance.json", options="driver=json")
+
+
+# Step 4 use the singelfile_converter
+dlite.storage_path.append(sampledir / "DLiteDataModelReaction.json")
+instance_from_singlefiledatanode = singlefile_converter(singlefileinst, parse_driver="json", options="mode=r")
+
+direct_instance = dlite.Instance.from_location("json", sample, options="mode=r")
+
+assert instance_from_singlefiledatanode.properties.keys() == direct_instance.properties.keys()
