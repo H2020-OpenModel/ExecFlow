85f207ccfa02976b6297ce74dba9fa09126d881d
Francesca.L.Bleken@sintef.no <francesca.l.bleken@sintef.no>
2024-07-03 14:20:23 +0200

    Added singlefileconverter function
    This is a function used by oteapi and dlite and includes
    a dlite plugin for a singlefileconverter.
    
    Note that since options are noe allowed in the from_bytes
    class method of dlite plugins the converter now makes a hack
    with a temporary file and then uses the load.

--- execflow/data/singlefile_converter.py
+++ execflow/data/singlefile_converter.py
@@ -4,42 +4,37 @@ to convert an AiiDA singlefiledatanode which
 has as content a DLite instance serialised as json.
 The AiiDA singlefile datanode is passed as
 a http://onto-ns.com/meta/2.0/core.singlefile.
-
-NB! To be moved to execflow
-
 """
 
-import dlite
+from __future__ import annotations
+
+from pathlib import Path
 import tempfile
+
+import dlite
+
+from execflow.data.setup_dlite import setup_dlite
+
+setup_dlite()
 
 
 def singlefile_converter(singlefile_instance, parse_driver="json", options=None):
     """The converter function"""
 
-    from execflow.data.setup_dlite import setup_dlite
-    setup_dlite()
-    print('hahaæ',dlite.python_storage_plugin_path)
     if singlefile_instance.meta.uri != "http://onto-ns.com/meta/2.0/core.singlefile":
-        raise ValueError(
-            f"Expected a singlefile instance, got {singlefile_instance.meta.uri}"
-        )
+        raise ValueError(f"Expected a singlefile instance, got {singlefile_instance.meta.uri}")
     buffer = singlefile_instance.content.tobytes()
     # save buffer to temporary file and load it as a DLite instance
     # use a temporary directory
 
     with tempfile.TemporaryDirectory() as temp_dir:
         temp_file = temp_dir + "/temp_file"
-        with open(temp_file, "wb") as f:
+        with Path.open(Path(temp_file), "wb") as f:
             f.write(buffer)
         parse_options = (
-            ";".join([f"driver={parse_driver}", f"options={options}"])
-            if options
-            else f"driver={parse_driver}"
+            ";".join([f"driver={parse_driver}", f"options={options}"]) if options else f"driver={parse_driver}"
         )
-        instance = dlite.Instance.from_location(
-            driver="singlefiledatanode", location=temp_file, options=parse_options
-        )
-    return instance
+        return dlite.Instance.from_location(driver="singlefiledatanode", location=temp_file, options=parse_options)
     # """The converter function"""
     # When it will be possible to pass options to the
     # from_bytes method of the DLite instance, the following
