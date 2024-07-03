b56b18ab2a86ab089c6fed271b4cd7bdeb94e247
Francesca.L.Bleken@sintef.no <francesca.l.bleken@sintef.no>
2024-07-03 14:29:29 +0200

    Added dliteplugin

---
+++ dlite_plugins/singlefiledatanode.py
@@ -0,0 +1,64 @@
+"""
+Dlite plugin to convert between DLite instances or files and AiiDA single file datanode.
+To be included in ExecFlow.
+
+"""
+
+from __future__ import annotations
+
+import json
+
+import dlite
+from dlite.options import Options
+
+
+class singlefiledatanode(dlite.DLiteStorageBase):
+    """General description of the Python storage plugin."""
+
+    def open(self, location, options=None):
+        """Open storage.
+
+        Arguments:
+            location:
+            options: Additional options for this storage driver.
+        """
+
+        self.options = Options(options, defaults="driver=json")
+        self.location = location
+
+    def load(self) -> dlite.Instance:
+        """
+        Load a state file and return it as a DLite instance
+
+        Returns:
+            A DLite Instance corresponding to the given filename.
+
+        """
+        return dlite.Instance.from_location(self.options.driver, self.location)
+
+    @classmethod
+    def from_bytes(cls, buffer):
+        """
+        From the content of the Abaqus output file
+        generate documented DLite output instance of
+        http://www.sintef.no/calm/0.1/AbaqusDeformationHistory
+
+        Arguments:
+            buffer: Bytes of bytearray to load instance from.
+            id: ID of instance to load. May be omitted if `buffer`
+                only holds one instance.
+        Returns:
+           New instance
+
+        """
+        return cls.create_instance(buffer)
+
+    @staticmethod
+    def create_instance(content):
+        """
+        read a state file and populate a DLite instance base on it
+        """
+        # read the file as a dictionary
+        dict_output = json.loads(content)
+
+        return dlite.Instance.from_dict(dict_output)
